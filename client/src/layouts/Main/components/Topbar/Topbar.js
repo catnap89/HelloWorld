ed64ebc5ee3c6f961f79cd163eddf5e9b8fb53d2
PCHIII <treyhelmer@gmail.com>
2019-10-27 00:40:41 -0400

    added login button to navbar with route

--- client/src/layouts/Main/components/Topbar/Topbar.js
+++ client/src/layouts/Main/components/Topbar/Topbar.js
@@ -3,7 +3,7 @@ import { Link as RouterLink } from 'react-router-dom';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
-import { AppBar, Toolbar, Badge, Hidden, IconButton } from '@material-ui/core';
+import { AppBar, Toolbar, Badge, Hidden, IconButton, Button } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import NotificationsIcon from '@material-ui/icons/NotificationsOutlined';
 import InputIcon from '@material-ui/icons/Input';
@@ -41,20 +41,7 @@ const Topbar = props => {
       <Toolbar>
         <RouterLink to="/">
 
-          {/* <Typography 
-            style = {{
-            // Some CSS
-              color: 'white',
-              fontWeight: 'bold'
-            }}
-            
-            variant="h2"
-          
-            
-          >
-                         Ch@
-          </Typography>
-               */}
+         
        <img
        style= {{height: '70px'}}
         alt="Logo"
@@ -62,6 +49,10 @@ const Topbar = props => {
           />
         </RouterLink>
         <div className={classes.flexGrow} />
+
+        <RouterLink to="/sign-in">
+        <Button style={{marginRight:'15px'}} variant="outlined" color="inherit">Login</Button>
+        </RouterLink> 
         <Hidden mdDown>
           <IconButton color="inherit">
             <Badge
